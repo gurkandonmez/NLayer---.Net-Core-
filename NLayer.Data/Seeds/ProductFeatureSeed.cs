e66b4237ffbc2f840c09617c08b61ba3ea997980
Gürkan Dönmez <donmezgurkan@gmail.com>
2023-03-13 23:06:08 +0300

    Seeds klasörü ve sınıflar oluşturuldu.

---
+++ NLayer.Data/Seeds/ProductFeatureSeed.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Data.Seeds
+{
+    public class ProductFeatureSeed : IEntityTypeConfiguration<ProductFeature>
+    {
+  
+
+        public void Configure(EntityTypeBuilder<ProductFeature> builder)
+        {
+            builder.HasData(new ProductFeature
+            {
+                Id = 1,
+                ProductId = 1,
+                Width = 20,
+                Height = 30,
+                Color = "Kırmızı"
+
+            },
+             new ProductFeature
+             {
+                 Id = 2,
+                 ProductId = 2,
+                 Width = 20,
+                 Height = 30,
+                 Color = "Pembe"
+
+             },
+             new ProductFeature
+             {
+                 Id = 3,
+                 ProductId = 3,
+                 Width = 20,
+                 Height = 30,
+                 Color = "Mavi"
+
+             },
+        new ProductFeature
+        {
+            Id = 4,
+            ProductId = 4,
+            Width = 20,
+            Height = 30,
+            Color = "Pembe"
+
+        });
+        }
+    }
+}
---
+++ NLayer.Data/Seeds/ProductSeed.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Data.Seeds
+{
+    public class ProductSeed : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasData(new Product
+            {
+                Id = 1,
+                CategoryId = 1,
+                Price = 100,
+                Stock = 20,
+                CreatedDate = DateTime.Now,
+                Name = "Kalem 1"
+            },
+            new Product
+            {
+                Id = 2,
+                CategoryId = 1,
+                Price = 200,
+                Stock = 20,
+                CreatedDate = DateTime.Now,
+                Name = "Kalem 2"
+            },
+            new Product
+            {
+                Id = 3,
+                CategoryId = 1,
+                Price = 300,
+                Stock = 20,
+                CreatedDate = DateTime.Now,
+                Name = "Kalem 3"
+            },
+        new Product
+        {
+            Id = 1,
+            CategoryId = 2,
+            Price = 300,
+            Stock = 20,
+            CreatedDate = DateTime.Now,
+            Name = "Defter 1"
+        },
+           new Product
+           {
+               Id = 2,
+               CategoryId = 2,
+               Price = 200,
+               Stock = 20,
+               CreatedDate = DateTime.Now,
+               Name = "Defter 2"
+           });
+        }
+    }
+}
